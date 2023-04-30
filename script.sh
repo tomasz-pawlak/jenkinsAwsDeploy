80c6fdf6ef81c089bf416145800c720bbf158237
tompaw <gurtoc@gmail.com>
2023-04-30 14:11:42 +0200

    some changes

--- script.sh
+++ script.sh
@@ -19,5 +19,5 @@ aws ecs register-task-definition --cli-input-json file://task-definition.json --
 REVISION=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_DEFAULT_REGION}" | jq .taskDefinition.revision`
 echo "REVISION= " "${REVISION}"
 #aws ecs update-service --cluster "${CLUSTER_NAME}" --service "${SERVICE_NAME}" --task-definition "${TASK_DEFINITION_NAME}":"${REVISION}" --desired-count "${DESIRED_COUNT}"
-aws ecs update-service --cluster "${CLUSTER_NAME}" --task-definition "${TASK_DEFINITION_NAME}":"${REVISION}" --desired-count "${DESIRED_COUNT}"
+aws ecs -cluster "${CLUSTER_NAME}" --task-definition "${TASK_DEFINITION_NAME}":"${REVISION}" --desired-count "${DESIRED_COUNT}"
 
